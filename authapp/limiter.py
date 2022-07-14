1304f20928c1190510fae8d448b537e2ebf6d76a
vladislavzujkov <velocrosss@gmail.com>
2022-07-14 21:23:01 +0300

    initial commit перенес результаты последнего решения в текущий спринт

--- authapp/limiter.py
+++ authapp/limiter.py
@@ -9,6 +9,7 @@ limiter = Limiter(
     key_func=get_remote_address,
     default_limits=config.get(os.environ.get('ENV', 'default')).REQUESTS_LIMITER,
     storage_uri=f'redis://{os.environ.get("REDIS_HOST","127.0.0.1")}:{os.environ.get("REDIS_PORT", 6379)}')
+    # storage_uri=f'redis://127.0.0.1:6379')
 
 
 def init_limiter(app: Flask):
--- authapp/oauth.py
+++ authapp/oauth.py
@@ -8,8 +8,8 @@ oauth = OAuth()
 class YandexConfig(BaseSettings):
 
     name: str = Field("yandex", env='yandex_agent_name')
-    client_id: str = Field(env="YANDEX_CLIENT_ID")
-    client_secret: str = Field(env="YANDEX_CLIENT_SECRET")
+    client_id: str = Field('7bc57d06e83d4a519dfb7d10402901d2', env="YANDEX_CLIENT_ID")
+    client_secret: str = Field('1938219175ec4c7f87f406aeace6951e',env="YANDEX_CLIENT_SECRET")
     api_base_url: AnyUrl = Field(
         "https://login.yandex.ru/", env="yandex_api_base_url"
     )
@@ -32,8 +32,8 @@ class GoogleScope(BaseModel):
 class GoogleConfig(BaseSettings):
     name: str = Field("google", env="google_agent_name")
     client_kwargs: GoogleScope = GoogleScope()
-    client_id: str = Field(env="GOOGLE_CLIENT_ID")
-    client_secret: str = Field(env="GOOGLE_CLIENT_SECRET")
+    client_id: str = Field('421420320201-ibdldjmoem7scfvek05m0vhb8516aq62.apps.googleusercontent.com',env="GOOGLE_CLIENT_ID")
+    client_secret: str = Field('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', env="GOOGLE_CLIENT_SECRET")
     server_metadata_url: AnyUrl = Field(
         "https://accounts.google.com/.well-known/openid-configuration",
         env="google_server_metadata_url"
