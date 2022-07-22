42cecc0feb765dbfa7e8a79c4a1196eb3ce93433
vladislavzujkov <velocrosss@gmail.com>
2022-07-22 18:44:16 +0300

    добавил backoof на подключение к CH добавил backoof на выполнение запроса к CH + logger

--- clickhouse/ETL/clickhouse_operate.py
+++ clickhouse/ETL/clickhouse_operate.py
@@ -1,11 +1,8 @@
 import logging
-from socket import error
-
 import backoff
+
 from clickhouse_driver import Client
-
-from clickhouse_driver.errors import NetworkError, CannotParseUuidError, ErrorCodes
-
+from clickhouse_driver.errors import NetworkError, CannotParseUuidError, Error
 from clickhouse.ETL.pkg.backoff_handlers.req_handler import create_backoff_hdlr
 
 logger = logging.getLogger(__name__)
@@ -17,7 +14,7 @@ back_off_hdlr = create_backoff_hdlr(logger)
 class ClickHouse:
     @backoff.on_exception(
         backoff.fibo,
-        exception=(ConnectionRefusedError, NetworkError, ConnectionError,BaseException),
+        exception=(ConnectionRefusedError, NetworkError,Error, ConnectionError,BaseException),
         max_time=BACKOFF_MAX_TIME,
         on_backoff=back_off_hdlr,
     )
@@ -26,8 +23,10 @@ class ClickHouse:
         client = Client(host='127.0.0.1')
         try:
             client.connection.connect()
+            logger.info('Connected to CLICKHOUSE')
             return client
-        except:
+        except Exception as ex:
+            logger.error(f'Connection refused error CLICKHOUSE {ex}')
             raise ConnectionRefusedError
 
 
@@ -35,12 +34,14 @@ class ClickHouse:
         backoff.fibo,
         exception=(NetworkError, CannotParseUuidError),
         max_time=BACKOFF_MAX_TIME,
-        # on_backoff=back_off_hdlr,
+        on_backoff=back_off_hdlr,
     )
     def ch_insert(self, insert_values: list):
         try:
             client = self.connection()
             client.execute(f"INSERT INTO default.views (film_id, user_id, timestamp) VALUES ",
                            (tuple(row) for row in insert_values))
-        except:
-            pass
+            logger.info(f'{len(insert_values)} added in CLICKHOUSE.')
+        except Exception as ex:
+            logger.error(f'Query error CLICKHOUSE {ex}')
+            raise Error
