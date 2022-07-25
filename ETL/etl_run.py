ba8d1b9d2c1d2516e0eb0c4d0ae4a72f85eff8f8
vladislavzujkov <velocrosss@gmail.com>
2022-07-26 00:42:43 +0300

    add clickhouse user

--- ETL/etl_run.py
+++ ETL/etl_run.py
@@ -31,9 +31,11 @@ class ETLProcessRunner:
                     continue
                 if len(insert_list) == config.KAFKA_BATCH_SIZE:
                     consumer.paused()
+                    logger.info("Kafka consumer paused")
                     clickhouse_operate.ch_insert(insert_values=insert_list)
                     insert_list.clear()
                     consumer.resume()
+                    logger.info("Kafka consumer resumed")
                 insert_list.append([*msg.key.decode("utf-8").split('_'), msg.value.decode("utf-8")])
 
 
